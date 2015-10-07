1327f6e33089f4dd562cafe18dea226b6f69cbff
fated-x <stephen.baker73@gmail.com>
2015-10-06 23:47:50 -0600

    Updated Wordpress to Version 4.3.1.

--- wp-content/themes/SymmetryFoundation/index.php
+++ wp-content/themes/SymmetryFoundation/index.php
@@ -19,18 +19,18 @@ get_header(); ?>
 <div class="row">
 	<div class="small-12 large-8 columns" role="main">
 
-	<?php if ( have_posts() ) : ?>
+	<?php if (have_posts()) : ?>
 
-		<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php do_action('foundationpress_before_content'); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format()); ?>
 		<?php endwhile; ?>
 
 		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php get_template_part('content', 'none'); ?>
 
-		<?php do_action( 'foundationpress_before_pagination' ); ?>
+		<?php do_action('foundationpress_before_pagination'); ?>
 
 	<?php endif;?>
 
--- wp-content/themes/SymmetryFoundation/library/custom-header.php
+++ wp-content/themes/SymmetryFoundation/library/custom-header.php
@@ -1,22 +1,21 @@
 <?php
 
 // The Header-image.
-	$defaults = array(
-
-	'default-image'   => '',
-	'width'           => 0,
-	'height'          => 0,
-	'flex-width'      => false,
-	'flex-height'     => false,
-	'uploads'         => true,
-	'random-default'  => false,
-	'header-text'     => false,
-	'default-text-color'  => '',
-	'wp-head'             => '',
-	'admin-head-callback' => '',
-	'admin-preview-callback' => '',
+$defaults = array(
+    'default-image'   => '',
+    'width'           => 0,
+    'height'          => 0,
+    'flex-width'      => true,
+    'flex-height'     => true,
+    'uploads'         => true,
+    'random-default'  => false,
+    'header-text'     => true,
+    'default-text-color'  => '000',
+    'wp-head'             => '',
+    'admin-head-callback' => '',
+    'admin-preview-callback' => '',
 );
 
-	add_theme_support( 'custom-header', $defaults );
+add_theme_support('custom-header', $defaults);
 
-	?>
+?>
